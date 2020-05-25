86eadd81561681af4d4e8c33edcab128394fd820
IEIrodov <nikhilindia30111999@gmail.com>
2020-05-25 12:49:36 +0530

    Testing Project

--- app.js
+++ app.js
@@ -134,9 +134,9 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.port;
+let port = process.env.PORT;
 if (port == null || port == "") {
-  port: 3000;
+  port = 3000;
 }
 
 app.listen(port, function () {
